a4e720bc1ab2d09435d60786dfdcdd82ab56b435
ginacuomo <g.cuomo-dannenburg18@imperial.ac.uk>
2023-04-04 13:30:45 +0100

    start of a function but n_infected zeros out at a certain point for some reason

--- 02.simulate_data.R
+++ 02.simulate_data.R
@@ -50,7 +50,7 @@ ggplot(data = drug_conc, aes(x = time/24, y = 1 - prob_susceptible)) +
 ## create a function to simulate test data for an arm of a trial
 simulate_trial_arm <- function(real_data, drug_arm, prob_susceptible, hourly_foi) {
   #' input: real data to gain time steps, which treatment arm, vector of hourly probability susceptibilities (drug_conc$prob_susceptible or equivalent)
-  #' input ctd: ths is a vector of 1s for the placebo arm and an hourly foi as a vector also to allow for variable foi in the future
+  #' input ctd: ths is a vector of 1s for the placebo arm and an hourly foi 
   #' process: extract time steps, initialise the number of patients, simulate time to next infections and then determine infections in time int from data
   #' output: a simulated dataframe for that treatment arm with the same data format as the original data
   ## initialise
@@ -62,8 +62,8 @@ simulate_trial_arm <- function(real_data, drug_arm, prob_susceptible, hourly_foi
   N_remaining <- N_patients
   time_infection <- NULL
   time_next_infection <- 0
-  for(i in 1:N_patients) {
-    time_next_infection <- time_next_infection + rexp(1, rate = N_remaining * hourly_foi)
+  for(i in 1:max_time) {
+    time_next_infection <- time_next_infection + rexp(1, rate = N_remaining * hourly_foi[i] * prob_susceptible[i])
     time_infection <- c(time_infection, time_next_infection)
     N_control_remaining <- N_control_remaining - 1
     if ((N_control_remaining == 0) || (time_next_infection > max_time)) {
@@ -71,14 +71,34 @@ simulate_trial_arm <- function(real_data, drug_arm, prob_susceptible, hourly_foi
     }
   }
   # time_infection is a vector of the times of every infection
-  
-  
+  N_remaining <- N_patients
+  new_infections <- 0
+  df_sim <- treat_data
+  df_sim$n_patients <- NA
+  df_sim$n_infected <- NA
+  for (i in 1:nrow(df_sim)) {
+    N_remaining <- N_remaining - new_infections
+    time0 <- treat_data$time[i]
+    time1 <- treat_data$time.1[i]
+    new_infections <- sum((time_infection > time0) & (time_infection <= time1))
+    ## simulated df
+    df_sim$time <- treat_data$time
+    df_sim$time.1 <- treat_data$time.1
+    df_sim$n_infected[i] <- sum((time_infection > time0) & (time_infection <= time1))
+    df_sim$n_patients[i] <- N_remaining
+    df_sim$treat_arm <- drug_arm
+  }
+  df_sim <- df_sim %>%
+    dplyr::select(c(n_patients, n_infected, time, time.1, treat_arm))
+  return(df_sim)
 }
 
+prob_susceptible_control <- rep(1, max(df_trial$time.1)+1)
+prob_susceptible_sp <- drug_conc$prob_susceptible
+length(prob_susceptible_control) == length(prob_susceptible_control) # sanity check
 
-
-
-
+## test function
+df_sim_control <- simulate_trial_arm(real_data = df_trial, drug_arm = 1, prob_susceptible = prob_susceptible_control, hourly_foi = hourly_foi)
 
 # simulate the control arm
 # TODO - this exponential method is technically correct, but to make more
